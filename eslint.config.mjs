c5e68a6b9d9210eb18d7b26d7a09a55fc467efa1
Zheng314159 <contemnewton@163.com>
2025-06-11 09:29:30 +0800

    eslint prettier eslint-config-prettier eslint-plugin-prettier @angular-eslint/builder @angular-eslint/eslint-plugin @angular-eslint/eslint-plugin-template @angular-eslint/template-parser

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import angularEslint from "@angular-eslint/eslint-plugin";
+import angularEslintTemplate from "@angular-eslint/eslint-plugin-template";
+import angularEslintParser from "@angular-eslint/template-parser";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  // Angular ESLint for TS files
+  {
+    files: ["**/*.ts"],
+    plugins: { '@angular-eslint': angularEslint },
+    extends: ["plugin:@angular-eslint/recommended"],
+    rules: {},
+  },
+  // Angular ESLint for HTML templates
+  {
+    files: ["**/*.html"],
+    plugins: { '@angular-eslint/template': angularEslintTemplate },
+    languageOptions: { parser: angularEslintParser },
+    extends: ["plugin:@angular-eslint/template/recommended"],
+    rules: {},
+  },
+]);
